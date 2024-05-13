fe946fedf31afdc4b8b8a304f2b00a30c9dd64c6
hyewon <first0210@naver.com>
2024-05-13 09:49:20 +0900

    Test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+	{
+	int A = 0;
+	int B = 0;
+
+        std::cin >> A;
+        std::cin >> B;
+
+ std::cout << A + B;
+std::cout << A - B;
+std::cout << A/ B;
+std::cout << A * B;
+
+return 0;
+	}
